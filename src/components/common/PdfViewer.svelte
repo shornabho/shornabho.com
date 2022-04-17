dbbc5af4c4efb37badf8b2b375274a9ba38802c5
Swarnava Ghosh <swarnava98@gmail.com>
2022-04-17 14:51:59 +0530

    fix(resume): Change Resume PDF URL to relative

--- src/components/common/PdfViewer.svelte
+++ src/components/common/PdfViewer.svelte
@@ -1,8 +1,5 @@
 <object data="Swarnava_Ghosh_Resume.pdf" type="application/pdf" frameborder="0">
-	<embed
-		src="http://localhost:3000/pdf/Swarnava_Ghosh_Resume.pdf#view=fit"
-		type="application/pdf"
-	/>
+	<embed src="/pdf/Swarnava_Ghosh_Resume.pdf#view=fit" type="application/pdf" />
 </object>
 
 <style>
