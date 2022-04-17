c57430d744d0cf4f5854e860aebd2e43e11e5744
Swarnava Ghosh <swarnava98@gmail.com>
2022-04-17 18:48:24 +0530

    fix(pdf-viewer): changes embed to object tag

--- src/components/common/PdfViewer.svelte
+++ src/components/common/PdfViewer.svelte
@@ -1,15 +1,18 @@
-<object data="Swarnava_Ghosh_Resume.pdf" type="application/pdf" frameborder="0">
-	<embed src="/pdf/Swarnava_Ghosh_Resume.pdf#view=fit" type="application/pdf" />
-</object>
+<object
+	title="Swarnava Ghosh - Resume"
+	data="/pdf/Swarnava_Ghosh_Resume.pdf#view=fit"
+	type="application/pdf"
+	frameborder="0"
+/>
 
 <style>
-	embed {
+	object {
 		height: 100vh;
 		width: 100%;
 	}
 
 	@media screen and (max-width: 768px) {
-		embed {
+		object {
 			height: 60vh;
 		}
 	}
